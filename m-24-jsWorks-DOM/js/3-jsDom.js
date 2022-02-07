75fdeae050172e91c85967f981aca0839c163f96
khadiza21 <bibikhadiza474@gmail.com>
2022-02-07 14:44:43 +0600

    js dom explore

--- m-24-jsWorks-DOM/js/3-jsDom.js
+++ m-24-jsWorks-DOM/js/3-jsDom.js
@@ -1,3 +1,4 @@
+//get elements by tagname
 let blogname = document.getElementsByTagName("h3");
 console.log(blogname);
 for (const h3 of blogname) {
@@ -10,14 +11,31 @@ for (const p of paraname) {
   console.log(p.innerText);
 }
 
-let b2Color = document.getElementById('b-2').style.backgroundColor='blue';
+//get element by id
+let b2Color = (document.getElementById("b-2").style.backgroundColor = "blue");
 console.log(b2Color);
 
+const b1 = document.getElementById("b-1");
+b1.style.textAlign = "center";
+b1.innerText = "My First Blog About Bot";
+b1.style.padding = "10px";
+b1.style.color = "white";
+b1.style.backgroundColor = "black";
+b1.style.margin = "40px";
 
-const b1 = document.getElementById('b-1');
-b1.style.textAlign = 'center';
-b1.innerText = 'My First Blog About Bot';
-b1.style.padding = '10px';
-b1.style.color = 'white';
-b1.style.backgroundColor = 'black';
-b1.style.margin = '40px';
+//explore get elements by className
+const blogs = document.getElementsByClassName("blog");
+for (const blog of blogs) {
+  console.log(blog);
+  blog.style.border = "3px solid black";
+  blog.style.borderRadius = "14px";
+  blog.style.padding = "15px";
+  blog.style.margin = "35px";
+}
+
+//explore query selector
+let p1 = document.querySelector("#blog p");
+const pAll = document.querySelectorAll("p");
+const pAll = document.querySelectorAll(".blog");
+
+//nodelist , htmlcollection,setAttribute,parentNode,childnodes
