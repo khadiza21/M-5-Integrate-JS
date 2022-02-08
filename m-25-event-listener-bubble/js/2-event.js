e93e5c63c84fd7483f79a50be4be95834f5b7cfb
khadiza21 <bibikhadiza474@gmail.com>
2022-02-08 13:33:11 +0600

    js dom event  explore

--- m-25-event-listener-bubble/js/2-event.js
+++ m-25-event-listener-bubble/js/2-event.js
@@ -1,3 +1,14 @@
-function makeGreen(){
-    document.body.style.backgroundColor = 'green';
+function makeGreen() {
+  document.body.style.backgroundColor = "green";
+}
+
+//handle purple btn click by setting function name
+const PurpleBtn = document.getElementById("make-purple");
+console.log(PurpleBtn);
+
+// just set the name of the function
+PurpleBtn.onclick = makePurple;
+
+function makePurple() {
+  document.body.style.backgroundColor = "purple";
 }
