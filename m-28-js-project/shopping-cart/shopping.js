7af853f093155f6fe4ae45b599ed834f647e2cfe
khadiza21 <bibikhadiza474@gmail.com>
2022-02-16 20:13:36 +0600

    string case change

--- m-28-js-project/shopping-cart/shopping.js
+++ m-28-js-project/shopping-cart/shopping.js
@@ -1,1 +1,29 @@
-console.log('shopping cart');
+// function updateProductNumber(product, price, isIncreasing) {
+//   const productInput = document.getElementById(product + "-number");
+//   let productNumber = productInput.value;
+//   if (isIncreasing == true) {
+//     productNumber = parseInt(productNumber) + 1;
+//   } else if (productNumber > 0) {
+//     productNumber = parseInt(productNumber) - 1;
+//   }
+
+//   productInput.value = productNumber;
+
+//   //update total
+//   const productTotal = document.getElementById(product + '-total');
+//   productTotal.innerText = productNumber*price;
+//   //calculate total
+
+// }
+
+// function calculateTotal(){
+//     const phoneTotal = get
+// }
+
+// phone increase decrease events
+document.getElementById("phone-plus").addEventListener("click", function () {
+  let phonePrice = document.getElementById("phone-price").innerText;
+  let totalPhone = document.getElementById("phones").innerText;
+
+  console.log( totalPhone);
+});
---
+++ m-29-arrayStringMethod/stringCase.js
@@ -0,0 +1,10 @@
+const anthem = "sona bangla";
+const search = "valo";
+
+const userInput = "bLackPiNk";
+const savedUserName = "blACkPInk";
+if (userInput.toLocaleLowerCase() == savedUserName) {
+  console.log("user exits");
+}
+console.log(userInput.toUpperCase());
+console.log(savedUserName.toLocaleLowerCase());
